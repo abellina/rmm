3e4ff4fd9c44b6bf7ccc7f1cee12abea3c2465e3
Alessandro Bellina <abellina@nvidia.com>
2025-01-08 13:33:39 -0800

    fix bug

--- include/rmm/mr/device/cuda_async_memory_resource.hpp
+++ include/rmm/mr/device/cuda_async_memory_resource.hpp
@@ -115,7 +115,7 @@ class cuda_async_memory_resource final : public device_memory_resource {
     } else {
       int cpu_numa_node_id;
       auto gpu_id   = rmm::get_current_cuda_device().value();
-      RMM_CUDA_TRY(cudaDeviceGetAttribute(&cpu_numa_node_id, cudaDevAttrNumaId, gpu_id); 
+      RMM_CUDA_TRY(cudaDeviceGetAttribute(&cpu_numa_node_id, cudaDevAttrNumaId, gpu_id)); 
       pool_props.location.id = cpu_numa_node_id;
     }
 

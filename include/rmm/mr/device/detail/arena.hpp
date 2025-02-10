543036bdbc3b2b0809ade0737692e10795c55604
Alessandro Bellina <abellina@nvidia.com>
2025-02-10 08:56:50 -0800

    remove logging

--- include/rmm/mr/device/detail/arena.hpp
+++ include/rmm/mr/device/detail/arena.hpp
@@ -222,7 +222,6 @@ class block final : public byte_span {
    */
   [[nodiscard]] std::pair<block, block> split(std::size_t bytes) const
   {
-    printf("at block::split\n");
     RMM_LOGGING_ASSERT(is_valid());
     RMM_LOGGING_ASSERT(size() > bytes);
     // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
@@ -284,11 +283,9 @@ class superblock final : public byte_span {
   {
     RMM_LOGGING_ASSERT(size >= minimum_size);
     RMM_LOGGING_ASSERT(size <= maximum_size);
-    printf("superblock_constructor\n");
     block b{pointer ,size};
     free_blocks_.insert(b);
     free_blocks_by_size_.insert(b);
-    printf("done superblock_constructor\n");
   }
 
   // Disable copy semantics.
@@ -344,7 +341,6 @@ class superblock final : public byte_span {
    */
   [[nodiscard]] bool fits(std::size_t bytes) const
   {
-    printf("superblock::fits\n");
     RMM_LOGGING_ASSERT(is_valid());
     if (free_blocks_by_size_.rbegin() == free_blocks_by_size_.rend()) {
       return false;
@@ -377,7 +373,6 @@ class superblock final : public byte_span {
   {
     RMM_LOGGING_ASSERT(is_valid());
     RMM_LOGGING_ASSERT(empty() && bytes >= minimum_size && size() >= bytes + minimum_size);
-    printf("at superblock split\n"); 
     // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     return {superblock{pointer(), bytes}, superblock{pointer() + bytes, size() - bytes}};
   }
@@ -404,7 +399,6 @@ class superblock final : public byte_span {
    */
   block first_fit(std::size_t size)
   {
-    printf("superblock::first_fit\n");
     NVTX3_FUNC_RANGE_IN(rmm::librmm_domain)
     RMM_LOGGING_ASSERT(is_valid());
     RMM_LOGGING_ASSERT(size > 0);
@@ -415,23 +409,16 @@ class superblock final : public byte_span {
 
     // Remove the block from the free list.
     auto const blk  = *iter;
-    printf("removing from free_blocks_by_size\n");
     free_blocks_by_size_.erase(blk);
-    printf("done removing from free_blocks_by_size\n");
     auto const next = free_blocks_.erase(iter);
-    printf("done removing from free_blocks_\n");
 
     if (blk.size() > size) {
       // Split the block and put the remainder back.
       auto const split = blk.split(size);
-      printf("inserting into free_blocks\n");
       free_blocks_.insert(next, split.second);
-      printf("inserting into free_blocks_by_size\n");
       free_blocks_by_size_.insert(split.second);
-      printf("done inserting into free_blocks_by_size\n");
       return split.first;
     }
-    printf("done with superblock::first_fit\n");
     return blk;
   }
 
@@ -471,7 +458,6 @@ class superblock final : public byte_span {
       free_blocks_.insert(iter, merged);
       free_blocks_by_size_.insert(merged);
     } else if (merge_prev) {
-      printf("merge_prev\n");
       auto const merged = previous->merge(blk);
       free_blocks_by_size_.erase(*previous);
       auto const iter   = free_blocks_.erase(previous);
@@ -633,15 +619,11 @@ class global_arena final {
    */
   void* allocate(std::size_t size)
   {
-    printf("at global_arena::allocate\n");
     RMM_LOGGING_ASSERT(handles(size));
     std::lock_guard lock(mtx_);
-    printf("at global_arena::allocate calling first_fit\n");
     auto sblk = first_fit(size);
     if (sblk.is_valid()) {
-      printf("at global_arena::allocate calling sblk.first_fit\n");
       auto blk = sblk.first_fit(size);
-      printf("at global_arena::allocate inserting sblk\n");
       superblocks_.insert(std::move(sblk));
       return blk.pointer();
     }
@@ -966,7 +948,6 @@ class arena {
    */
   block first_fit(std::size_t size)
   {
-    printf("arena::first_fit\n");
     rmm::scoped_range rng{"arena first_fit"};
     auto const iter = std::find_if(superblocks_.cbegin(),
                                    superblocks_.cend(),
@@ -975,12 +956,9 @@ class arena {
       return {}; 
     }
 
-    printf("arena::first_fit found sblk\n");
     auto sblk      = std::move(superblocks_.extract(iter).value());
-    printf("arena::first_fit moved sblk\n");
     auto const blk = sblk.first_fit(size);
     superblocks_.insert(std::move(sblk));
-    printf("done arena::first_fit\n");
     return blk;
   }
 

e8ae91ee4b607a905c8f11b9dcaf5ede279dff82
Alessandro Bellina <abellina@nvidia.com>
2025-03-18 03:43:22 -0700

    disable hw decompress

--- include/rmm/mr/device/cuda_async_memory_resource.hpp
+++ include/rmm/mr/device/cuda_async_memory_resource.hpp
@@ -107,8 +107,8 @@ class cuda_async_memory_resource final : public device_memory_resource {
     auto is_gpu = (location_type.value_or(mem_location_type::device)) == mem_location_type::device;
     // Construct explicit pool
     cudaMemPoolProps pool_props{};
-    printf("and setting the hw decompres props\n");
-    pool_props.usage = cudaMemPoolCreateUsageHwDecompress;
+    printf("NOT setting the hw decompres props\n");
+    //pool_props.usage = cudaMemPoolCreateUsageHwDecompress;
     pool_props.allocType   = cudaMemAllocationTypePinned;
     pool_props.handleTypes = static_cast<cudaMemAllocationHandleType>(
       export_handle_type.value_or(allocation_handle_type::none));
